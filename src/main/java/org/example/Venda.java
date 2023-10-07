2e9daeb357dbb225e43f7b4eee50a850ff9912da
Kielson Zinn da Silva <kielsonzinn>
2023-10-07 20:15:51 -0300

    create code class Venda

--- src/main/java/org/example/Venda.java
+++ src/main/java/org/example/Venda.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,44 +12,72 @@ public class Venda {
     private List<Recebimento> recebimentos = new ArrayList<>();
 
     public Integer getProximoSequencialItem() {
-        //TODO IMPLEMENTAR
-        return 0;
+
+        Integer max = 0;
+
+        for (Item item : itens) {
+            if (item.getSequencial() > max) {
+                max = item.getSequencial();
+            }
+        }
+
+        return max + 1;
+
     }
 
     public void adicionarItem(Item item) {
-        //TODO IMPLEMENTAR
+        itens.add(item);
     }
 
     public void removerItem(Integer sequencial) {
-        //TODO IMPLEMENTAR
+        for (Item item : itens) {
+            if (item.getSequencial().equals(sequencial)) {
+                itens.remove(item);
+                break;
+            }
+        }
     }
 
     public BigDecimal getTroco() {
-        //TODO IMPLEMENTAR
-        return BigDecimal.valueOf(0.00);
+        return getTotal().subtract(getPagamentoTotal()).multiply(BigDecimal.valueOf(-1)).setScale(2, RoundingMode.HALF_UP);
     }
 
     public void adicionarRecebimento(Recebimento recebimento) {
-        //TODO IMPLEMENTAR
+        recebimentos.add(recebimento);
     }
 
     public void removerRecebimento(Integer sequencial) {
-        //TODO IMPLEMENTAR
+        for (Recebimento recebimento : recebimentos) {
+            if (recebimento.getSequencial().equals(sequencial)) {
+                recebimentos.remove(recebimento);
+                break;
+            }
+        }
     }
 
     public BigDecimal getTotal() {
-        //TODO IMPLEMENTAR
-        return BigDecimal.valueOf(0.00);
+        BigDecimal total = new BigDecimal(0.00);
+
+        for (Item item : itens) {
+            total = total.add(item.getPrecoTotal());
+        }
+
+        return total.setScale(2, RoundingMode.HALF_UP);
     }
 
     public BigDecimal getPagamentoPendente() {
-        //TODO IMPLEMENTAR
-        return BigDecimal.valueOf(0.00);
+        return getTotal().subtract(getPagamentoTotal()).setScale(2, RoundingMode.HALF_UP);
     }
 
     public BigDecimal getPagamentoTotal() {
-        //TODO IMPLEMENTAR
-        return BigDecimal.valueOf(0.00);
+
+        BigDecimal total = new BigDecimal(0.00);
+
+        for (Recebimento recebimento : recebimentos) {
+            total = total.add(recebimento.getPagamento());
+        }
+
+        return total.setScale(2, RoundingMode.HALF_UP);
     }
 
 }
